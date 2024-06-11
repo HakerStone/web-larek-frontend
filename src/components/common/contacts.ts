f611a3ff7d442229f80d3c2ea5c76e9fba58c39e
HakerStone <102145491+HakerStone@users.noreply.github.com>
2024-06-11 03:25:39 +0300

    Удаление лишних ранее использовавшихся файлов

--- src/components/common/contacts.ts
+++
@@ -1,9 +0,0 @@
-import {Form} from "./form";
-
-export class Contacts extends Form {
-  init() {
-    super.init();
-    this.inputs = this.configure( ['email', 'phone']);
-    this.element('button').bindEmitter(this.events).bindEvent('click', 'show-success');
-  }
-}
