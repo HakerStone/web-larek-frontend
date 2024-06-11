0787cd242c78b623e460d6166a1fd01437e40147
HakerStone <102145491+HakerStone@users.noreply.github.com>
2024-06-11 03:24:12 +0300

    Удаление лишних ранее использовавшихся файлов

--- src/components/common/success.ts
+++
@@ -1,8 +0,0 @@
-import {Component, elementMode} from "../base/Component";
-
-export class Success extends Component<HTMLDivElement> {
-  init() {
-    this.element('button', elementMode.independent).bindEmitter(this.events).bindEvent('click', 'close')
-    this.on('close', () => this.events.emit('clear-data'))
-  }
-}
